7081e61c7764b698d20080661130188270e76723
nevenRakonic <neven.rakonic@gmail.com>
2013-12-21 17:56:26 +0100

    after 6

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,15 +1,31 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category, Page
+
+#### HELPER FUNCTIONS ###
+
+def decode_url(url):
+    return url.replace('_',' ')
+
+def encode_url(url):
+    return url.replace(' ','_')
+
+### VIEWS ###
 
 def index(request):
     # Request the context of the request.
     # The context contains information such as the client's machine details, for example.
     context = RequestContext(request)
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    for category in category_list:
+        category.url = encode_url(category.name)
+
+    page_list = Page.objects.order_by('-views')[:5]    
+    context_dict["pages"] =  page_list
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -19,4 +35,20 @@ def index(request):
 def about_page(request):
     context = RequestContext(request)
     return render_to_response('rango/about.html', context)
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+    category_name = decode_url(category_name_url)
+    context_dict = {'category_name': category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
+
 
